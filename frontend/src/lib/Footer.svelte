67684f7a20e7217965c3f66eb59117b2cdae2d68
prince kumar <agarwal8789@gmail.com>
2024-07-20 22:52:17 +0530

    commit made on brand-page dashboard

---
+++ frontend/src/lib/Footer.svelte
@@ -0,0 +1,14 @@
+<footer class="bg-white rounded-md shadow dark:bg-gray-800 fixed bottom-0 w-screen">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center gap-4">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">TeamC™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-1 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 gap-4">
+        <li>
+            <p class="hover:underline">Terms and conditions</p>
+        </li>
+        <li>
+            <p class="hover:underline">Privacy Policy</p>
+        </li>
+    </ul>
+    </div>
+</footer>
---
+++ frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,59 @@
+
+ 
+<aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-[400] transition-transform -translate-x-full sm:translate-x-0 pt-16" aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+       <ul class="space-y-2 font-medium">
+          <li>
+             <a href="/brand-setup" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span class="ms-3">Brand Profile Setup</span>
+             </a>
+          </li>
+          <li>
+             <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span class="flex-1 ms-3 whitespace-nowrap">Loyalty Point Issuance</span>
+             </a>
+          </li>
+          <li>
+             <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span class="flex-1 ms-3 whitespace-nowrap">Loyalty Point Management</span>
+             </a>
+          </li>
+          <li>
+             <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span class="flex-1 ms-3 whitespace-nowrap">Account Settings</span>
+             </a>
+          </li>
+          <li>
+             <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
+             </a>
+          </li>
+          
+       </ul>
+    </div>
+ </aside>
+ 
+ <div class="p-4 sm:ml-64">
+    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+         <h1 class="text-xl font-semibold text-gray-900 dark:text-white">Dashboard</h1>
+         <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Welcome back, John Doe!</p>
+    </div>
+    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <h1 class="text-xl font-semibold text-gray-900 dark:text-white">Dashboard</h1>
+        <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Welcome back, John Doe!</p>
+   </div>
+   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+    <h1 class="text-xl font-semibold text-gray-900 dark:text-white">Dashboard</h1>
+    <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Welcome back, John Doe!</p>
+</div>
+<div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+    <h1 class="text-xl font-semibold text-gray-900 dark:text-white">Dashboard</h1>
+    <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Welcome back, John Doe!</p>
+</div>
+<div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+    <h1 class="text-xl font-semibold text-gray-900 dark:text-white">Dashboard</h1>
+    <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Welcome back, John Doe!</p>
+</div>
+
+
+ </div>
