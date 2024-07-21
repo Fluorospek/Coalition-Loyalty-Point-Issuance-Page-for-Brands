c32414ebaba0f093e00d73d027319f499fcc792e
Shivamani Yamana <shivamaniyamana2003@gmail.com>
2024-07-21 12:09:20 +0530

    Added Navbar with highlighting current page

--- frontend/src/lib/Header.svelte
+++ frontend/src/lib/Header.svelte
@@ -1,18 +1,54 @@
+<script>
+	import { page } from '$app/stores';
+
+	console.log($page.route.id);
+	let navs = [
+		{
+			title: 'Home',
+			href: '/'
+		},
+		{
+			title: 'Login',
+			href: '/login'
+		},
+		{
+			title: 'Register',
+			href: '/register'
+		},
+		{
+			title: 'Dashboard',
+			href: '/dashboard'
+		}
+	];
+</script>
+
 <nav class="bg-white border-gray-200 dark:bg-blue-400">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="/home" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://tsoc.dev/TSoC_Favicon.svg" class="h-8" alt="Flowbite Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BlockChain</span>
-      </a>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-blue-400 md:dark:bg-blue-400 dark:border-gray-700">
-        <li>
-            <a href="/home" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-900" aria-current="page">Home</a>
-        </li>
-        <li>
-            <a href="/login" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
-        </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+		<a href="/home" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<img src="https://tsoc.dev/TSoC_Favicon.svg" class="h-8" alt="Flowbite Logo" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+				>BlockChain</span
+			>
+		</a>
+		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+			<ul
+				class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-blue-400 md:dark:bg-blue-400 dark:border-gray-700"
+			>
+				{#each navs as { title, href }}
+					<li>
+						<a {href} class="nonactive active" class:active={$page.route.id === href}>{title}</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<style>
+	.nonactive {
+		@apply block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-900 hover:md:dark:text-white;
+	}
+	.active {
+		@apply text-white dark:text-white md:dark:text-white;
+	}
+</style>
