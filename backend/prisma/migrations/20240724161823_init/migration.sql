0055e876904b3853fe2c03c881d5ea6e66ce1add
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2024-07-24 22:08:01 +0530

    Added endpoint to check details of previously generated token

--- backend/prisma/migrations/20240724161823_init/migration.sql
+++ backend/prisma/migrations/20240724161823_init/migration.sql
@@ -21,8 +21,25 @@ CREATE TABLE "Brand" (
     CONSTRAINT "Brand_pkey" PRIMARY KEY ("brandId")
 );
 
+-- CreateTable
+CREATE TABLE "BrandTokens" (
+    "brandTokenId" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "pointName" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "BrandTokens_pkey" PRIMARY KEY ("brandTokenId")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "BrandTokens_userId_key" ON "BrandTokens"("userId");
+
 -- AddForeignKey
 ALTER TABLE "Brand" ADD CONSTRAINT "Brand_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BrandTokens" ADD CONSTRAINT "BrandTokens_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
