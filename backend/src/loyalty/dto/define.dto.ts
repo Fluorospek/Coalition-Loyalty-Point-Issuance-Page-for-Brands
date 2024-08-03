3d6a37f8da758d421aada501cd7716b6dfda4b3a
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2024-08-03 15:24:04 +0530

    Dto updated with Api property

--- backend/src/loyalty/dto/define.dto.ts
+++ backend/src/loyalty/dto/define.dto.ts
@@ -1,11 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class DefineDto{
-
+    @ApiProperty({type:String,description:"Name of the point"})
     @IsString()
     @IsNotEmpty()
     pointName:string
 
+    @ApiProperty({type:String,description:"Symbol of the point"})
     @IsString()
     @IsNotEmpty()
     symbol:string
