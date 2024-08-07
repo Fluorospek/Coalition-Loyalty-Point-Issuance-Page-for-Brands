7bc51eb711cfd58413b79279c8937e45f76040b2
prince kumar <agarwal8789@gmail.com>
2024-08-07 12:54:32 +0530

    commit made make the footer transparent

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,12 +1,9 @@
-<footer class="bg-white rounded-md shadow dark:bg-gray-800 fixed bottom-0 w-screen">
+<footer class="bg-transparent dark:bg-transparent fixed bottom-0 w-screen">
 	<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center gap-4">
-		<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-			>© 2024 <a href="https://timechainlabs.io/" class="hover:underline">TeamC™</a>. All Rights
-			Reserved.
+		<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+			© 2024 <a href="https://timechainlabs.io/" class="hover:underline">TeamC™</a>. All Rights Reserved.
 		</span>
-		<ul
-			class="flex flex-wrap items-center mt-1 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 gap-4"
-		>
+		<ul class="flex flex-wrap items-center mt-1 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 gap-4">
 			<li>
 				<p class="hover:underline">Terms and conditions</p>
 			</li>
